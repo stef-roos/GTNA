8ccf016ce26fe9411bd297fe0270e9b0c53b5b13
Stefanie Roos <stef-roos@web.de>
2012-05-14 17:57:21 +0200

    communities by clustering

---
+++ src/gtna/transformation/communities/CommunityDetectionLPAClustering.java
@@ -0,0 +1,211 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * CommunityDetectionLPAClustering.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: stef;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna.transformation.communities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import gtna.communities.CommunityList;
+import gtna.graph.Graph;
+import gtna.graph.Node;
+import gtna.graph.sorting.NodeSorting;
+import gtna.transformation.Transformation;
+import gtna.util.Util;
+import gtna.util.parameter.IntParameter;
+import gtna.util.parameter.Parameter;
+
+/**
+ * @author stef
+ *
+ */
+public class CommunityDetectionLPAClustering extends Transformation {
+
+	private int iterationLimitFactor;
+
+	/**
+	 * Standard constructor. Only argument is the factor that determines the
+	 * maximum number of iterations that is being done. This maximum number is
+	 * calculated by multiplying the number of nodes with this factor. With a
+	 * value above 50, it should only stop infinite loopings while not affecting
+	 * normal community detection. However, even if it stops infinite loops,
+	 * depending on the number of nodes this might still take a very long time.
+	 * 
+	 * @param factor
+	 *            The maximum number of iterations is determined by factor *
+	 *            nodes.
+	 */
+	public CommunityDetectionLPAClustering(int limitFactor) {
+		super("COMMUNITY_DETECTION_LPA_CLUSTER", new Parameter[] { new IntParameter(
+				"LIMIT_FACTOR", limitFactor) });
+		this.iterationLimitFactor = limitFactor;
+	}
+
+	@Override
+	public Graph transform(Graph g) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		int[] labels = labelPropagationAlgorithm(g.getNodes());
+		HashMap<Integer, Integer> labelCommunityMapping = Util
+				.mapLabelsToCommunities(labels);
+
+		for (Node n : g.getNodes()) {
+			map.put(n.getIndex(),
+					labelCommunityMapping.get(labels[n.getIndex()]));
+		}
+
+		g.addProperty(g.getNextKey("COMMUNITIES"),
+				new CommunityList(map));
+		return g;
+	}
+
+	/**
+	 * Assigns labels to nodes using the label propagation algorithm.
+	 * 
+	 * @param nodes
+	 * @return an array containing the label of each node
+	 */
+	private int[] labelPropagationAlgorithm(Node[] nodes) {
+		Random rand = new Random();
+		boolean finished = false;
+		int[] labels = new int[nodes.length];
+		double[][] weights = this.initWeights(nodes);
+		for (int i = 0; i < labels.length; i++)
+			labels[i] = i;
+
+		int count = 0;
+
+		while (!finished) {
+
+			Node[] X = NodeSorting.random(nodes, rand);
+			finished = true;
+			for (Node x : X) {
+				ArrayList<Integer> maxLabels = selectMaxLabels(
+						x.getOutgoingEdges(), labels, weights[x.getIndex()]);
+				if (!maxLabels.isEmpty()) {
+					int maxLabel = maxLabels
+							.get(rand.nextInt(maxLabels.size()));
+					if (!maxLabels.contains(labels[x.getIndex()]))
+						finished = false;
+					labels[x.getIndex()] = maxLabel;
+				}
+			}
+			count++;
+
+			if (count > nodes.length * iterationLimitFactor)
+				finished = true;
+		}
+		return labels;
+	}
+
+	/**
+	 * Selects the labels occurring with the highest frequency among the
+	 * neighboring nodes.
+	 * 
+	 * @param nodes
+	 * @param labels
+	 * @return list of labels occuring with the highest frequency
+	 */
+	private ArrayList<Integer> selectMaxLabels(int[] nodes, int[] labels, double[] weights) {
+		HashMap<Integer, Double> counter = new HashMap<Integer, Double>(); // <label,
+																				// count>
+		ArrayList<Integer> maxLabels = new ArrayList<Integer>();
+		double maxCount = 0;
+
+		for (int  i = 0;i < nodes.length; i++) {
+			int label = labels[nodes[i]];
+			double count = counter.containsKey(label) ? counter.get(label) + weights[i] : weights[i];
+			counter.put(label, count);
+			if (count > maxCount) {
+				maxCount = count;
+			}
+		}
+
+		for (int label : counter.keySet()) {
+			if (counter.get(label) == maxCount) {
+				maxLabels.add(label);
+			}
+		}
+
+		return maxLabels;
+	}
+
+	@Override
+	public boolean applicable(Graph g) {
+		return true;
+	}
+	
+	private double[][] initWeights(Node[] nodes){
+		double[][] weights = new double[nodes.length][];
+		int count1,count2,n1;
+		int[] out,out2;
+		for (int i = 0; i < nodes.length; i++){
+			weights[i] = new double[nodes[i].getInDegree()];
+			out = nodes[i].getOutgoingEdges();
+			for (int j=0; j < out.length; j++){
+				if (nodes[i].getInDegree() == 1){
+					weights[i][j] = 1;
+					break;
+				}
+				n1 = out[j];
+				count1 = 0;
+				for (int n2: out){
+					if (n1 != n2){
+						if (nodes[n1].hasNeighbor(n2)){
+							count1++;
+						}
+					}
+				}
+				count2 = 0;
+				out2 = nodes[n1].getOutgoingEdges();
+				for (int n2: out2){
+					if (i != n2){
+						if (nodes[i].hasNeighbor(n2)){
+							count2++;
+						}
+					}
+				}
+				weights[i][j] = count1/(double)(nodes[i].getInDegree()-1); 
+				if (nodes[n1].getInDegree() > 1){
+				   weights[i][j] += count2/(double)(nodes[n1].getInDegree()-1);
+				}		
+			}
+		}
+		
+		return weights;
+	}
+
+}
